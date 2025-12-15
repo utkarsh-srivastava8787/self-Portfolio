41ddcfdd4ede28bf7d33f397f79f8a133071613d
Utkarsh Srivastava <utkarshsrivastava8787@gmail.com>
2025-12-15 23:17:50 +0530

    remove env variables

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -23,5 +23,5 @@ export const router = createBrowserRouter(
       ],
     },
   ],
-  { basename: `/${repoName}` }
+  { basename: `/` }
 );
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
-    base: `/`,
+      base: `/`,
     plugins: [react(), tailwindcss()],
     server: {
       open: true,
